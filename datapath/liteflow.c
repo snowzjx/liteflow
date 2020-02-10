ec726f243affa179482802adb917baf20530c310
Junxue Zhang <snow.zjx@outlook.com>
2020-02-10 04:27:32 +0000

    Rename

--- datapath/liteflow.c
+++ datapath/liteflow.c
@@ -5,19 +5,19 @@
 static int
 __init liteflow_module_init(void)
 {
-    printk(KERN_INFO "LiteFlow init...\n");
+    printk(KERN_INFO "liteflow init...\n");
     return 0;
 }
 
 static void
 __exit liteflow_module_exit(void)
 {
-    printk(KERN_INFO "LiteFlow exit...\n");
+    printk(KERN_INFO "liteflow exit...\n");
 }
 
 module_init(liteflow_module_init);
 module_exit(liteflow_module_exit);
 
-MODULE_DESCRIPTION("LiteFlow");
+MODULE_DESCRIPTION("liteflow");
 MODULE_AUTHOR("Junxue ZHANG");
 MODULE_LICENSE("GPL");
