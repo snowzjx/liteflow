b530a5467fe0195241bb0186a9c18bbaf225e2a6
HydraZeng <zcl@ZCL-THINKPAD.localdomain>
2020-02-13 13:15:10 +0800

    add metric loader

--- datapath/liteflow_tcp.c
+++ datapath/liteflow_tcp.c
@@ -8,24 +8,53 @@
 
 #include "linux/liteflow.h"
 
+#define S_TO_US 1000000
+
 #define LF_TCP_APP_ID 1 // CC applications will be given ID 1
 // Here we allow different connection can use different models, just change the APP_ID per connection
 // In this implementation, connection information is put in the input of the NN
 
-// NUM_OF_INPUT_METRICS metrics will be passed to NN
+// NUM_OF_INPUT_METRICS metrics in a history record
 // such as acked bytes, ecn bytes and so on
 #define INPUT_METRICS_POS_BYTES_ACKED 0
-#define INPUT_METRICS_POS_ECN_BYTES 1
+#define INPUT_METRICS_POS_PACKETS_ACKED 1
+#define INPUT_METRICS_POS_ECN_BYTES 2
+#define INPUT_METRICS_POS_ECN_PACKETS 3
+#define INPUT_METRICS_POS_RTT_SAMPLE_US 4
+#define INPUT_METRICS_POS_DELIVERED 5
+#define INPUT_METRICS_POS_INTERVAL_US 6
+#define INPUT_METRICS_POS_RATE_OUTGOING 7
 // More can be added
-#define NUM_OF_INPUT_METRICS 2
+#define NUM_OF_INPUT_METRICS 8
 
 #define HISTORY_LEN 3
+
+// NUM_OF_GLOBAL_STATS per connection
+#define GLOBAL_STATS_POS_MIN_RTT_US 0
+// More can be added
+#define NUM_OF_GLOBAL_STATS 1
 
 #define OUTPUT_RATE 0
 #define NUM_OF_OUTPUT_VALUE 1
 
 struct lf_tcp_metric {
-    s64 values[NUM_OF_INPUT_METRICS]; // store collected metrics
+    // // newly acked, in-order bytes
+    // u32 bytes_acked;
+    // // newly acked, in-order packets
+    // u32 packets_acked;
+    // // bytes corresponding to ecn-marked packets
+    // u32 ecn_bytes;
+    // // ecn-marked packets
+    // u32 ecn_packets;
+    // // a recent sample of the round-trip time
+    // s64 rtt_sample_us;
+    // // newly sended packets
+    // s32 delivered;
+    // // time for delivered
+    // s64 interval_us;
+    // // sample of the sending rate, bytes / s
+    // u32 rate_outgoing;
+    s64 values[NUM_OF_INPUT_METRICS];
 };
 
 struct lf_tcp_internal {
@@ -35,6 +64,7 @@ struct lf_tcp_internal {
     u64 dest_port;
     u64 last_snd_una;
     u32 current_pointer;
+    s64 global_stats[NUM_OF_GLOBAL_STATS];
     struct lf_tcp_metric* metrics; // ring buffer
 };
 
@@ -50,6 +80,48 @@ static inline void lf_set_rate (struct sock *sk, u32 rate) {
     sk->sk_pacing_rate = rate;
 }
 
+static int rate_sample_valid(const struct rate_sample *rs) {
+    int ret = 0;
+    if (rs->delivered <= 0)
+    ret |= 1;
+    if (rs->interval_us <= 0)
+    ret |= 1 << 1;
+    if (rs->rtt_us <= 0)
+    ret |= 1 << 2;
+    return ret;
+}
+
+static int load_metric(struct sock *sk, const struct rate_sample *rs) {
+    struct tcp_sock *tp;
+    struct lf_tcp_internal *ca;
+    struct lf_tcp_metric metric;
+    
+    int measured_valid_rate = rate_sample_valid(rs);
+    if (measured_valid_rate != 0) {
+        return -1;
+    }
+    
+    tp = tcp_sk(sk);
+    ca = inet_csk_ca(sk);
+    metric = ca->metrics[ca->current_pointer];
+
+    // kernel version == 4.15.0
+    // compatible to most of versions
+    metric.values[INPUT_METRICS_POS_RTT_SAMPLE_US] = rs->rtt_us;
+    metric.values[INPUT_METRICS_POS_DELIVERED] = rs->delivered;
+    metric.values[INPUT_METRICS_POS_INTERVAL_US] = rs->interval_us;
+    metric.values[INPUT_METRICS_POS_RATE_OUTGOING] = (s64) rs->delivered * tp->mss_cache * S_TO_US;
+    do_div(metric.values[INPUT_METRICS_POS_RATE_OUTGOING], rs->interval_us);
+    
+    if (ca->global_stats[GLOBAL_STATS_POS_MIN_RTT_US] == -1 
+        || ca->global_stats[GLOBAL_STATS_POS_MIN_RTT_US] > rs->rtt_us) 
+    {
+        ca->global_stats[GLOBAL_STATS_POS_MIN_RTT_US] = rs->rtt_us;
+    }
+
+    return 0;
+}
+
 static void lf_tcp_conn_init(struct sock *sk) {
     struct lf_tcp_internal *ca;
     struct tcp_sock *tp;
@@ -63,6 +135,7 @@ static void lf_tcp_conn_init(struct sock *sk) {
     ca->dest_ip = tp->inet_conn.icsk_inet.inet_daddr;
     ca->dest_port = tp->inet_conn.icsk_inet.inet_dport;
     ca->last_snd_una = tp->snd_una;
+    ca->global_stats[GLOBAL_STATS_POS_MIN_RTT_US] = -1;
 
     ca->current_pointer = 0;
     ca->metrics = kmalloc(sizeof(struct lf_tcp_metric) * HISTORY_LEN, GFP_KERNEL);
@@ -78,7 +151,14 @@ static void lf_tcp_conn_init(struct sock *sk) {
 
 static void lf_tcp_conn_release(struct sock *sk)
 {
-    printk(KERN_INFO "Free flow handled by liteflow tcp kernel...\n");
+    struct lf_tcp_internal *ca;
+
+    printk(KERN_INFO "Free flow handled by liteflow tcp kernel...\n");  
+
+    ca = inet_csk_ca(sk);
+    if (ca->metrics != NULL) {
+        kfree(ca->metrics);
+    }
 }
 
 // Learn from CCP
@@ -99,7 +179,7 @@ static u32 lf_tcp_conn_undo_cwnd(struct sock *sk)
 // and set sending rate based on returned rate
 static void lf_tcp_conn_nn_control(struct sock *sk, const struct rate_sample *rs) 
 {
-    int ret, metric_pos, value_pos, pos = 0;
+    int ret, global_stats_pos, metric_pos, value_pos, pos = 0;
     u32 output_rate;
     struct lf_tcp_internal *ca;
     s64 nn_input[NUM_OF_INPUT_METRICS * HISTORY_LEN];
@@ -110,9 +190,19 @@ static void lf_tcp_conn_nn_control(struct sock *sk, const struct rate_sample *rs
         printk(KERN_ERR "Current flow is not managed by liteflow tcp kernel!\n");
         return;
     }
+
+    ret = load_metric(sk, rs);
+    if(ret < 0) {
+        return;
+    }
     
     // Prepare input vector
     // Performance is not good
+    for (global_stats_pos = 0; global_stats_pos < NUM_OF_GLOBAL_STATS; ++global_stats_pos) {
+        nn_input[pos] = ca->global_stats[global_stats_pos];
+        pos++;
+    }
+
     for (metric_pos = 0; metric_pos < HISTORY_LEN; ++metric_pos) {
         for (value_pos = 0; value_pos < NUM_OF_INPUT_METRICS; ++value_pos) {
             nn_input[pos] = ca->metrics[(ca->current_pointer + metric_pos) % HISTORY_LEN].values[value_pos];
@@ -127,6 +217,8 @@ static void lf_tcp_conn_nn_control(struct sock *sk, const struct rate_sample *rs
         output_rate = nn_output[OUTPUT_RATE];
         lf_set_rate(sk, output_rate);
     }
+
+    lf_increase_pointer(ca);
 }
 
 static void lf_tcp_conn_in_ack_event(struct sock *sk, u32 flags) 
@@ -135,7 +227,7 @@ static void lf_tcp_conn_in_ack_event(struct sock *sk, u32 flags)
     // More can be added
     const struct tcp_sock *tp;
     struct lf_tcp_internal *ca;
-    u32 acked_bytes;
+    u32 acked_bytes, acked_packets;
     
     tp = tcp_sk(sk);
     ca = inet_csk_ca(sk);
@@ -146,21 +238,24 @@ static void lf_tcp_conn_in_ack_event(struct sock *sk, u32 flags)
     }
 
     acked_bytes = tp->snd_una - ca->last_snd_una;
+    acked_packets = (u64)acked_bytes / tp->mss_cache;
     ca->last_snd_una = tp->snd_una;
 
     ca->metrics[ca->current_pointer].values[INPUT_METRICS_POS_BYTES_ACKED] = acked_bytes;
+    ca->metrics[ca->current_pointer].values[INPUT_METRICS_POS_PACKETS_ACKED] = acked_packets;
     if (flags & CA_ACK_ECE) {
         ca->metrics[ca->current_pointer].values[INPUT_METRICS_POS_ECN_BYTES] = acked_bytes;
+        ca->metrics[ca->current_pointer].values[INPUT_METRICS_POS_ECN_PACKETS] = acked_packets;
     } else {
         ca->metrics[ca->current_pointer].values[INPUT_METRICS_POS_ECN_BYTES] = 0;
+        ca->metrics[ca->current_pointer].values[INPUT_METRICS_POS_ECN_PACKETS] = 0;
     }
-    lf_increase_pointer(ca);
 }
 
 
 struct app lf_tcp_app = {
     .appid = LF_TCP_APP_ID,
-    .input_size = NUM_OF_INPUT_METRICS,
+    .input_size = NUM_OF_GLOBAL_STATS + NUM_OF_INPUT_METRICS * HISTORY_LEN,
     .output_size = NUM_OF_OUTPUT_VALUE,
 };
 
