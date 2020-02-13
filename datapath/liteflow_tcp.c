d1f45591da4e66458e39d5ad1ee15b1ad51a861f
Junxue Zhang <snow.zjx@outlook.com>
2020-02-13 12:20:34 +0000

    Refactor

--- datapath/liteflow_tcp.c
+++ datapath/liteflow_tcp.c
@@ -7,35 +7,9 @@
 #include <linux/inet_diag.h>
 
 #include "linux/liteflow.h"
+#include "linux/liteflow_tcp.h"
 
 #define S_TO_US 1000000
-
-#define LF_TCP_APP_ID 1 // CC applications will be given ID 1
-// Here we allow different connection can use different models, just change the APP_ID per connection
-// In this implementation, connection information is put in the input of the NN
-
-// NUM_OF_INPUT_METRICS metrics in a history record
-// such as acked bytes, ecn bytes and so on
-#define INPUT_METRICS_POS_BYTES_ACKED 0
-#define INPUT_METRICS_POS_PACKETS_ACKED 1
-#define INPUT_METRICS_POS_ECN_BYTES 2
-#define INPUT_METRICS_POS_ECN_PACKETS 3
-#define INPUT_METRICS_POS_RTT_SAMPLE_US 4
-#define INPUT_METRICS_POS_DELIVERED 5
-#define INPUT_METRICS_POS_INTERVAL_US 6
-#define INPUT_METRICS_POS_RATE_OUTGOING 7
-// More can be added
-#define NUM_OF_INPUT_METRICS 8
-
-#define HISTORY_LEN 3
-
-// NUM_OF_GLOBAL_STATS per connection
-#define GLOBAL_STATS_POS_MIN_RTT_US 0
-// More can be added
-#define NUM_OF_GLOBAL_STATS 1
-
-#define OUTPUT_RATE 0
-#define NUM_OF_OUTPUT_VALUE 1
 
 struct lf_tcp_metric {
     s64 values[NUM_OF_INPUT_METRICS];
